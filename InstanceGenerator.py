0a05c777a36d43965eda59819a0cf69d096870b8
Paulaner99 <rikipaolini99@gmail.com>
2022-05-12 18:01:11 +0200

    Update InstanceGenerator.py

--- InstanceGenerator.py
+++ InstanceGenerator.py
@@ -5,7 +5,7 @@ import os
 
 os.mkdir('new_inst')
 
-for file in os.listdir("./Inst"):
+for file in os.listdir("Inst/"):
 
   # Reading the file
   f = open('Inst/' + file, "r")
@@ -15,36 +15,36 @@ for file in os.listdir("./Inst"):
   # Splitting the file by rows
   content = text.split('\n')
 
-  # Number of items to be delivered
+  # Number of objects to deliver
   n = int(content[1])
   
   # Delivery coordinates of each object
   x = [int(el) for el in content[4].split(' ')]
   y = [int(el) for el in content[5].split(' ')]
-
+  
   # Intantiating the distance matrix
-  dist = np.zeros(shape=(n,n), dtype=int)
+  dist = np.zeros(shape=(n+1,n+1), dtype=int)
 
   # Filling the matrix with the Manhattan distance between each pair of objects
-  for i in range(n):
-    for j in range(i, n):
+  for i in range(n+1):
+    for j in range(i, n+1):
       dist[i, j] = dist[j, i] = abs(x[i] - x[j]) + abs(y[i] - y[j])
 
   # Creating a file in the new folder
   f = open('new_inst/' + file + '.dzn', "w")
 
   # Writing the new file .dzn of the instance
-  f.write('n = ' + content[0] + ';\n')
-  f.write('m = ' + content[1] + ';\n')
-  f.write('l = [| '); f.writelines([L for L in content[2]]); f.write(' |];\n')
-  f.write('s = [| '); f.writelines([L for L in content[3]]); f.write(' |];\n')
+  f.write('m = ' + content[0] + ';\n')
+  f.write('n = ' + content[1] + ';\n')
+  f.write('l = ['); f.writelines([str(int(L)) + ', ' for L in content[2].split(' ')]); f.write('];\n')
+  f.write('s = ['); f.writelines([str(int(L)) + ', ' for L in content[3].split(' ')]); f.write('];\n')
   
   # Writing the matrix values in the file
-  f.write('D = [| ') 
-  for i in range(n):
-    for j in range(n):
-      f.write(str(dist[i, j]) + ' ')
-    f.write('|\n')
+  f.write('D = [|') 
+  for i in range(n+1):
+    for j in range(n+1):
+      f.write(str(dist[i, j]) + ', ')
+    f.write('|')
   f.write('];\n')
 
   # Closing the new file
