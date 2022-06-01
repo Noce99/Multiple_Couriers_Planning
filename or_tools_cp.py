b7d6f27af67ee8da3430a82bafa8e88dae793924
Enrico <enrico.mannocci99@gmail.com>
2022-06-01 14:49:27 +0200

    Zio po funziona or_tools_cp

--- or_tools_cp.py
+++ or_tools_cp.py
@@ -40,68 +40,123 @@ s = [int(L) for L in content[3].split(',')]
 x = [int(L) for L in content[4].split(',')]
 y = [int(L) for L in content[5].split(',')]
 
-N = n + 2*m
+N = n + 1
 ITEMS = list(range(n))
 NODES = list(range(N))
 COURIERS = list(range(m))
-START = list(range(n, n+m))
-END = list(range(n+m, N))
 
-D = np.array([[0 for j in range(n+1)] for i in range(n+1)])
+D = np.array([[0 for j in range(N)] for i in range(N)])
 
 for i in range(n+1):
     for j in range(n+1):
         D[i, j] = abs(x[i] - x[j]) + abs(y[i] - y[j])
 
-
-
 model = cp_model.CpModel()
 
-successor = []
-courier = []
+# VARIABLES
+table = []
+for k in COURIERS:
+    rows = []
+    for i in NODES:
+        cols = []
+        for j in NODES:
+            cols.append(model.NewBoolVar(name=f't[{k},{i},{j}]'))
+        rows.append(cols)
+    table.append(rows)
 
-for i in NODES:
-    successor.append(model.NewIntVar(0, N-1, f'successor[{i}]'))
-
-for i in NODES:
-    courier.append(model.NewIntVar(0, m-1, f'courier[{i}]'))
 
 #successor = [model.NewIntVar(0, N-1, 'successor%i' % i) for i in NODES]
 #courier = [model.NewIntVar(0, N-1, 'courier%i' % i) for i in NODES]
 
 # ------ initialization constraints ----
 
-for i in NODES:
-    model.AddCircuit((i, successor[i], 1))
-
-for i in range(n+m+1, N-1):
-    model.AddElement(i, successor, i-m+1)
-
-model.Add(successor[N] == n+1)
-
-for i in START:
-    model.Add(courier[i] == i - n)
-for i in END:
-    model.Add(courier[i] == i - n - m)
-
-# ------ COURIER CONSTRAINTS ----
+# Ongi oggetto raggiunto esattamente una volta
 for i in ITEMS:
-    model.Add(courier[successor[i]] == courier[i])
+    model.AddExactlyOne([table[k][i][j] for k in COURIERS for j in NODES if j != i])
+    model.AddExactlyOne([table[k][j][i] for k in COURIERS for j in NODES if j != i])
 
-# ------ LOAD CONSTRAINTS ----
-for i in COURIERS:
-    model.Add(np.sum(s[j] for j in ITEMS if courier[j] == i) <= l[i])
+for k in COURIERS:
+    model.Add(table[k][N-1][N-1] == 0)
 
-# ------ OBJECTIVE FUNCTION ---- %
-obj = np.sum(D[i, successor[i]] for i in NODES)
+# constraint forall(k in COURIERS)(sum(i in NODES, j in ITEMS)(table[i, j, k]*s[j]) <= l[k]);
+for k in COURIERS:
+    somma = 0
+    for j in range(N-1):
+        somma += sum([table[k][i][j] for i in NODES if i != j])*s[j]
+    model.Add(somma <= l[k])
 
-solver.Minimize(obj)
 
+for k in COURIERS:
+    arcs = []
+    for i in NODES:
+        for j in NODES:
+            arcs.append((i, j, table[k][i][j]))
+    model.AddCircuit(arcs)
+
+
+
+model.Minimize(cp_model.LinearExpr.Sum([table[k][i][j]*D[i, j] for k in COURIERS for i in NODES for j in NODES]))
 solver = cp_model.CpSolver()
+# Massimo 5 minuti
+solver.parameters.max_time_in_seconds = 300.0
 status = solver.Solve(model)
 
+
 if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
+    for k in COURIERS:
+        print(f"\nCourier{k}:\n")
+        for i in NODES:
+            for j in NODES:
+                print(f'{solver.Value(table[k][i][j])}', end=" ")
+            print("")
+        print("--------------------------------------")
     print(f'Minimum of objective function: {solver.ObjectiveValue()}\n')
-    print(f'successor = {solver.Value(successor)}')
+    for k in COURIERS:
+        somma = 0
+        for j in range(N-1):
+            somma += sum([solver.Value(table[k][i][j]) for i in NODES if i != j])*s[j]
+        print("sum[{}] = {} and l[{}] = {}".format(k, somma, k, l[k]))
+    for i in range(len(x)):
+        plt.gca().add_patch(plt.Circle((x[i],y[i]), 0.1, color='r'))
+    x1s = []
+    y1s = []
+    x2s = []
+    y2s = []
+    color_i = 1
+    color = 'C1'
+    for k in range(m):
+        x1s.clear()
+        y1s.clear()
+        x2s.clear()
+        y2s.clear()
+        for i in range(N):
+            for j in range(N):
+                if i != j:
+                    if solver.Value(table[k][i][j]) == 1:
+                        x1s.append(x[i])
+                        y1s.append(y[i])
+                        x2s.append(x[j]-x[i])
+                        y2s.append(y[j]-y[i])
+        plt.quiver(x1s, y1s, x2s, y2s, angles='xy', scale_units='xy', color=[color], scale=1, width=0.003)
+        color_i += 1
+        color = 'C'+str(color_i)
+    plt.xlim(min(x)-2,max(x)+2)
+    plt.ylim(min(y)-2,max(y)+2)
+    plt.show()
 else:
     print('No solution found.')
+
+"""
+plt.scatter(x[:-1], y[:-1], c='b')
+for i in range(n):
+    plt.annotate(f'q{i}', (x[i]+0.05, y[i]+0.05))
+
+for k in COURIERS:
+    for i in NODES:
+        for j in NODES:
+            if solver.Value(table[k][i][j]) == 1:
+                plt.plot([x[i], x[j]], [y[i], y[j]], c='y', alpha=0.4)
+
+plt.plot(x[-1], y[-1], c='r', marker='s')
+plt.show()
+"""
